dabc5f1fd1b69100ca646edebdc86927ca730188
Gerardo <torresanibal388@gmail.com>
2026-02-22 16:57:49 -0400

    Se crearon las migraciones de las tablas iniciales

--- database/migrations/2026_02_22_173647_create_materias_table.php
+++ database/migrations/2026_02_22_173647_create_materias_table.php
@@ -6,13 +6,13 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    
+    /**
+     * Run the migrations.
+     */
     public function up(): void
     {
-        Schema::create('roles', function (Blueprint $table) {
+        Schema::create('materias', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->unique();
-            $table->string('description')->nullable();
             $table->timestamps();
         });
     }
@@ -22,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-      Schema::dropIfExists('roles');
+        Schema::dropIfExists('materias');
     }
 };
